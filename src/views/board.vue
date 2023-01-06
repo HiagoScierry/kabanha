4c8be9635a6aa00399ea08d4eab10db8a30b3aa0
Hiago Moreira <47890178+HiagoScierry@users.noreply.github.com>
2023-01-06 17:53:40 -0300

    ajust layout in board

--- src/views/board.vue
+++ src/views/board.vue
@@ -1,55 +1,116 @@
 <template>
-  <div class="flex justify-evenly items-center">
-    <div>
-      <h4>BACKLOG</h4>
-      <ul>
-        <li
-          v-for="(item, index) in arrBackLog"
-          :key="item.id"
-          @click="removeFromIndex(index)"
-        >
-          {{ item.title }}
-        </li>
-      </ul>
+  <div class="flex justify-evenly items-center h-5/6">
+    <div class="border rounded-md flex flex-col items-center w-1/4 h-5/6 p-2">
+      <h2 class="font-bold uppercase mb-4">backlog</h2>
+      <draggrable
+        v-model="arrBackLog"
+        group="kanban"
+        @start="true"
+        @end="false"
+        item-key="id"
+        class="w-full overflow-y-scroll"
+      >
+        <template #item="{ element }">
+          <div
+            class="flex justify-between w-full h-24 p-2 border border-slate-50 rounded-md mb-2"
+          >
+            <div class="flex flex-col justify-between items-start">
+              <p class="break-all">{{ element.title }}</p>
+              <div class="flex items-center justify-start">
+                <i class="fa-solid fa-user mr-2"></i>
+                <p>{{ element.assignee }}</p>
+              </div>
+            </div>
+            <div class="flex flex-col justify-between items-end">
+              <i class="fa-solid fa-xmark hover:text-red-600"></i>
+              <div class="flex items-center">
+                <i class="fa-regular fa-clock mr-2"></i>
+                <p>{{ new Date(element.dueDate).toLocaleDateString() }}</p>
+              </div>
+            </div>
+          </div>
+        </template>
+      </draggrable>
     </div>
-    <div>
-      <h4>PROGRESS</h4>
-      <ul>
-        <li
-          v-for="(item, index) in arrInProgress"
-          :key="item.id"
-          @click="removeFromIndex(index)"
-        >
-          {{ item.title }}
-        </li>
-      </ul>
+    <div class="border rounded-md flex flex-col items-center w-1/4 h-5/6 p-2">
+      <h2 class="font-bold uppercase mb-4">backlog</h2>
+      <draggrable
+        v-model="arrInProgress"
+        group="kanban"
+        @start="true"
+        @end="false"
+        item-key="id"
+        class="w-full overflow-y-scroll"
+      >
+        <template #item="{ element }">
+          <div
+            class="flex justify-between w-full h-24 p-2 border border-slate-50 rounded-md mb-2"
+          >
+            <div class="flex flex-col justify-between items-start">
+              <p class="break-all">{{ element.title }}</p>
+              <div class="flex items-center justify-start">
+                <i class="fa-solid fa-user mr-2"></i>
+                <p>{{ element.assignee }}</p>
+              </div>
+            </div>
+            <div class="flex flex-col justify-between items-end">
+              <i class="fa-solid fa-xmark hover:text-red-600"></i>
+              <div class="flex items-center">
+                <i class="fa-regular fa-clock mr-2"></i>
+                <p>{{ new Date(element.dueDate).toLocaleDateString() }}</p>
+              </div>
+            </div>
+          </div>
+        </template>
+      </draggrable>
     </div>
-    <div>
-      <h4>DONE</h4>
-      <ul>
-        <li
-          v-for="(item, index) in arrDone"
-          :key="item.id"
-          @click="removeFromIndex(index)"
-        >
-          {{ item.title }}
-        </li>
-      </ul>
+
+    <div class="border rounded-md flex flex-col items-center w-1/4 h-5/6 p-2">
+      <h2 class="font-bold uppercase mb-4">backlog</h2>
+      <draggrable
+        v-model="arrDone"
+        group="kanban"
+        @start="true"
+        @end="false"
+        item-key="id"
+        class="w-full overflow-y-scroll"
+      >
+        <template #item="{ element }">
+          <div
+            class="flex justify-between w-full h-24 p-2 border border-slate-50 rounded-md mb-2"
+          >
+            <div class="flex flex-col justify-between items-start">
+              <p class="break-all">{{ element.title }}</p>
+              <div class="flex items-center justify-start">
+                <i class="fa-solid fa-user mr-2"></i>
+                <p>{{ element.assignee }}</p>
+              </div>
+            </div>
+            <div class="flex flex-col justify-between items-end">
+              <i class="fa-solid fa-xmark hover:text-red-600"></i>
+              <div class="flex items-center">
+                <i class="fa-regular fa-clock mr-2"></i>
+                <p>{{ new Date(element.dueDate).toLocaleDateString() }}</p>
+              </div>
+            </div>
+          </div>
+        </template>
+      </draggrable>
     </div>
   </div>
-  <button @click="routerToAdd()">ADD</button>
+  <button @click="add">ADD</button>
 </template>
 
 <script lang="ts">
-//import draggable
+//import draggrable
 import { store, storeMethods } from '@/stores/kanban';
-import draggable from 'vuedraggable';
+import draggrable from 'vuedraggable';
 import type { IBacklogItem } from '../interfaces/item';
 
 export default {
   name: 'kanban-board',
   components: {
-    draggable,
+    draggrable,
   },
   data(): {
     arrBackLog: IBacklogItem[];
@@ -67,11 +128,17 @@ export default {
     routerToAdd: function () {
       this.$router.push('/task');
     },
-    add: function () {
-      storeMethods.addItem();
-    },
     removeFromIndex: function (index: number) {
       storeMethods.removeItemFronIndex(index);
+    },
+    add: function () {
+      console.log('add');
+      storeMethods.addItem({
+        title: 'item.title',
+        description: 'item.description',
+        priority: 'item.priority',
+        dueDate: new Date(),
+      });
     },
   },
 };
